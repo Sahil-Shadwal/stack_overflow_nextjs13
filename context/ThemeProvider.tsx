74ce4bd9328a75496f2e47da96d4081a185a8b55
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-10-10 20:53:29 +0530

    feat/added navbar

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -12,18 +12,22 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
 
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
       setMode("dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
       document.documentElement.classList.remove("dark");
     }
   };
 
-  //   useEffect(() => {
-  //     handleThemeChange();
-  //   }, [mode]);
+  useEffect(() => {
+    handleThemeChange();
+  }, [mode]);
 
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
