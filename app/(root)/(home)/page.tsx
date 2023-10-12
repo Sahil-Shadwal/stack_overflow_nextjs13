ab60222f1e69aca1a009330423d79c19475ce385
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-10-13 00:23:40 +0530

    feat/ added filters with responsiveness , used shadcn

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import LocalSearchBar from "../../../components/shared/search/LocalSearchBar";
+import Filter from "@/components/shared/Filter";
+import { HomePageFilters } from "@/constants/filters";
+import HomeFilters from "@/components/home/HomeFilters";
 
 export default function Home() {
   return (
@@ -21,8 +24,14 @@ export default function Home() {
           placeholder="Search for questions"
           otherClasses="flex-1"
         />
-        Filters
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
+
+      <HomeFilters />
     </>
   );
 }
