615e54cb2df518aa92d91ac2cef896fbdd2f99cb
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-10-14 18:28:28 +0530

    feat/ finished homepage, added utilities

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,34 +5,43 @@ import Filter from "@/components/shared/Filter";
 import { HomePageFilters } from "@/constants/filters";
 import HomeFilters from "@/components/home/HomeFilters";
 import NoResult from "@/components/shared/NoResult";
+import QuestionCard from "@/components/cards/QuestionCard";
 
 const questions = [
-  // {
-  //   _id: 1,
-  //   title: "Cascading Deletes in SQLAlchemy?",
-  //   tags: [
-  //     { _id: 1, name: "python" },
-  //     { _id: 2, name: "sql" },
-  //   ],
-  //   author: "Sahil Shadwal",
-  //   upvotes: 10,
-  //   views: 100,
-  //   answers: 2,
-  //   createdAt: "2021-05-01T12:00:00.000Z",
-  // },
-  // {
-  //   _id: 2,
-  //   title: "How to center a div?",
-  //   tags: [
-  //     { _id: 1, name: "css" },
-  //     { _id: 2, name: "sql" },
-  //   ],
-  //   author: "Sahil Shadwal",
-  //   upvotes: 10,
-  //   views: 100,
-  //   answers: 2,
-  //   createdAt: "2021-05-01T12:00:00.000Z",
-  // },
+  {
+    _id: "1",
+    title: "Cascading Deletes in SQLAlchemy?",
+    tags: [
+      { _id: "1", name: "python" },
+      { _id: "2", name: "sql" },
+    ],
+    author: {
+      _id: "1",
+      name: "Sahil Shadwal",
+      picture: "path/to/picture.jpg",
+    },
+    upvotes: 10,
+    views: 1000000,
+    answers: [{}, {}], // Placeholder objects for answers
+    createdAt: new Date("2023-05-01T12:00:00.000Z"),
+  },
+  {
+    _id: "2",
+    title: "How to center a div?",
+    tags: [
+      { _id: "3", name: "css" },
+      { _id: "4", name: "html" },
+    ],
+    author: {
+      _id: "2",
+      name: "Sahil Shadwal",
+      picture: "path/to/another-picture.jpg",
+    },
+    upvotes: 15,
+    views: 120,
+    answers: [{}, {}], // Placeholder objects for answers
+    createdAt: new Date("2023-06-15T10:30:00.000Z"),
+  },
 ];
 
 export default function Home() {
@@ -65,7 +74,19 @@ export default function Home() {
 
       <div className="mt-10 flex w-full flex-col gap-6">
         {questions.length > 0 ? (
-          questions.map((question) => "QuestionCard")
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
         ) : (
           <NoResult
             title="There&rsquo;s no question to show"
