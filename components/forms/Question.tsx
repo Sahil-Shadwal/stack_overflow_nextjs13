1b83cd040ee9ebbb2a04f2648276ca1a3d33ff16
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-10-20 23:43:13 +0530

    feat: connected mongodb database

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -20,6 +20,7 @@ import { useForm } from "react-hook-form";
 import { QuestionsSchema } from "@/lib/validation";
 import { Badge } from "../ui/badge";
 import Image from "next/image";
+import { createQuestion } from "@/lib/actions/question.action";
 
 const type: any = "create";
 
@@ -38,12 +39,13 @@ const Question = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof QuestionsSchema>) {
+  async function onSubmit(values: z.infer<typeof QuestionsSchema>) {
     setIsSubmitting(true);
 
     try {
       // make an async call to your API -> create a question
       // contain all form data
+      await createQuestion({});
       // navigate to home page
     } catch (error) {
     } finally {
@@ -130,6 +132,8 @@ const Question = () => {
                     // @ts-ignore
                     (editorRef.current = editor)
                   }
+                  onBlur={field.onBlur}
+                  onEditorChange={(content) => field.onChange(content)}
                   initialValue=""
                   init={{
                     height: 350,
