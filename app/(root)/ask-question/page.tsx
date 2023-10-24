d3c26cfca944857345ae70ea4cfb237c192ecfca
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-10-24 11:48:49 +0530

    feat: added webhooks

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,13 +1,12 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
-// import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-  // const { userId } = auth();
+  const { userId } = auth();
 
-  const userId = "12345";
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
