a874886175d9f3f02a8fb29171b9c0f7dad4e85f
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-10-26 00:31:34 +0530

    Implementation of create answer action

---
+++ lib/actions/answer.action.ts
@@ -0,0 +1,28 @@
+" use server";
+
+import Answer from "@/database/answer.model";
+import { connectToDatabase } from "../mongoose";
+import { CreateAnswerParams } from "./shared.types";
+import Question from "@/database/question.model";
+import { revalidatePath } from "next/cache";
+
+export async function createAnswer(params: CreateAnswerParams) {
+  try {
+    connectToDatabase();
+
+    const { content, author, question, path } = params;
+
+    const newAnswer = new Answer({ content, author, question });
+
+    // Add the answer to the question's answers array
+    await Question.findByIdAndUpdate(question, {
+      $push: { answers: newAnswer._id },
+    });
+
+    // Todo: Add interaction...
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
