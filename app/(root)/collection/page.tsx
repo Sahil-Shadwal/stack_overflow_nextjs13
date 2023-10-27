3e5ba2c74915dd2b98d9536383da7baac15fb5af
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-10-27 12:14:20 +0530

    fix : typescript error

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -35,7 +35,7 @@ export default async function Home() {
 
       <div className="mt-10 flex w-full flex-col gap-6">
         {result.questions.length > 0 ? (
-          result.questions.map((question) => (
+          result.questions.map((question: any) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
