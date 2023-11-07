9693822ca4ffd786d05e0ac7785a0f308ebcde0b
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-11-07 23:43:32 +0530

    added popular tags action using aggregate

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -3,17 +3,12 @@ import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
 import { getHotQuestions } from "@/lib/actions/question.action";
-
-const popularTags = [
-  { _id: "1", name: "javascript", totalQuestions: 5 },
-  { _id: "2", name: "react", totalQuestions: 3 },
-  { _id: "3", name: "next", totalQuestions: 4 },
-  { _id: "4", name: "redux", totalQuestions: 2 },
-  { _id: "5", name: "java", totalQuestions: 5 },
-];
+import { getTOpPopularTags } from "@/lib/actions/tag.actions";
 
 const RightSidebar = async () => {
   const hotQuestions = await getHotQuestions();
+  const popularTags = await getTOpPopularTags();
+
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
@@ -47,7 +42,7 @@ const RightSidebar = async () => {
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.totalQuestions}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
