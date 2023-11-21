730e2610f6f3985331c6c0d8d34ba974c7868dc5
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-11-22 00:24:45 +0530

    Add user model and update interaction model

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -208,6 +208,13 @@ export async function downvoteQuestion(params: QuestionVoteParams) {
     }
 
     // Increment author's reputation
+    await User.findByIdAndUpdate(userId, {
+      $inc: { reputation: hasdownVoted ? -2 : 2 },
+    });
+
+    await User.findByIdAndUpdate(question.author, {
+      $inc: { reputation: hasdownVoted ? -10 : 10 },
+    });
 
     revalidatePath(path);
   } catch (error) {
